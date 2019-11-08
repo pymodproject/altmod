62955dd9e6aab65387f157e298379155709b08f8
giacomojanson <giacomo.janson@uniroma1.it>
2019-11-08 22:37:00 +0100

    Update and rename examples/altmod_basic.py to examples/basic_example/altmod_basic.py

--- examples/basic_example/altmod_basic.py
+++ examples/basic_example/altmod_basic.py
@@ -1,15 +1,19 @@
+import os
 from modeller import *
 from modeller.automodel import *
 
 from altmod import Automodel_statistical_potential
 
 
+example_dirpath = os.path.dirname(__file__)
 env = environ()
+env.io.atom_files_directory.append(example_dirpath)
 
 # This will build 5 models of the TvLDH protein, including in the MODELLER
 # objective function DOPE terms with a weight of 0.5 (the default behaviour of
 # the 'Automodel_statistical_potential' class).
-a = Automodel_statistical_potential(env, alnfile='tar_tem_alignment.ali',
+a = Automodel_statistical_potential(env,
+                                    alnfile=os.path.join(example_dirpath, 'tar_tem_alignment.ali'),
                                     knowns='1bdm', sequence='TvLDH')
 
 # Uncomment the line below to employ a more thorough molecular dynamics refinement phase.
